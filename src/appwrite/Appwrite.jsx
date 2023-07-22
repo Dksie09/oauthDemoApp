2f85461225ac5770ade3e08e42e64b557bc8812e
Dakshi <dakshiegoel@gmail.com>
2023-07-22 07:43:10 +0530

    added .env.example

--- src/appwrite/Appwrite.jsx
+++ src/appwrite/Appwrite.jsx
@@ -2,36 +2,35 @@ import { Client, Account } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("64b6b7747af59bd49ab0");
+  .setEndpoint(process.env.REACT_APP_ENDPOINT)
+  .setProject(process.env.REACT_APP_PROJECT_ID);
 
 const account = new Account(client);
 
 export const sdk = {
-    register: async (success, failure) => {
-        account.createOAuth2Session('github', success, failure)
-    },
+  register: async (success, failure) => {
+    account.createOAuth2Session("github", success, failure);
+  },
 
-    getAccount: async () => {
-        return await account.get()
-    },
+  getAccount: async () => {
+    return await account.get();
+  },
 
-    getSession: async () => {
-        return await account.getSession('current')
-    },
+  getSession: async () => {
+    return await account.getSession("current");
+  },
 
-    getGithubData: async () => {
-        const promise = await account.getSession('current')
-        console.log(promise.providerAccessToken)
+  getGithubData: async () => {
+    const promise = await account.getSession("current");
+    console.log(promise.providerAccessToken);
 
-        const response = await fetch('https://api.github.com/user', {
-            headers: { Authorization: `token ${promise.providerAccessToken}` }
-        })
-        return await response.json()
-        
-    },
+    const response = await fetch("https://api.github.com/user", {
+      headers: { Authorization: `token ${promise.providerAccessToken}` },
+    });
+    return await response.json();
+  },
 
-    logout: async () => {
-        return await account.deleteSession('current');   
-    }
+  logout: async () => {
+    return await account.deleteSession("current");
+  },
 };
