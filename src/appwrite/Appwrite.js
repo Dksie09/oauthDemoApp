47e5573beb33af6c2dadfe2309f746027e5e6f0c
Dakshi <dakshiegoel@gmail.com>
2023-07-27 17:19:47 +0530

    fixed some issues

--- src/appwrite/Appwrite.js
+++ src/appwrite/Appwrite.js
@@ -2,28 +2,33 @@ import { Client, Account } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(process.env.REACT_APP_ENDPOINT)
-  .setProject(process.env.REACT_APP_PROJECT_ID);
+  .setEndpoint(`${process.env.REACT_APP_ENDPOINT}`)
+  .setProject(`${process.env.REACT_APP_PROJECT_ID}`);
 
 const account = new Account(client);
 
 export const sdk = {
   register: async (success, failure) => {
+    //create oauth2 session
     account.createOAuth2Session("github", success, failure);
   },
 
   getAccount: async () => {
+    //get account data
     return await account.get();
   },
 
   getSession: async () => {
+    //get current session data
     return await account.getSession("current");
   },
 
   getGithubData: async () => {
+    //get user's provider access token
     const promise = await account.getSession("current");
     console.log(promise.providerAccessToken);
 
+    //make a request to github api
     const response = await fetch("https://api.github.com/user", {
       headers: { Authorization: `token ${promise.providerAccessToken}` },
     });
